18f2346dead67f508061815a35f39d7b8d53fd98
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2019-08-01 22:48:25 -0400

    added model classes

---
+++ models/incident.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+const ObjectId = Schema.Types.ObjectId
+
+const incidentSchema = new Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    status: {type: String, required: true},
+    createdOn: {type: Date, required: false},
+    modifiedOn: {type: Date, required: false, default: null},
+    creator: {type: ObjectId, ref: 'User'}
+})
+
+module.exports = mongoose.model('Incident', incidentSchema)
