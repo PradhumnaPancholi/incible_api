2205c6e31f70e153b3af40ffccde2cc917a4612f
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-08-30 21:29:14 -0400

    unit testing setup, Note:- requires debugging

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const router = require("./routes/routes");
 const keys = require("./config/keys");
+const userRoutes = require("./routes/userRoutes");
+const incidentRoutes = require("./routes/incidentRoutes");
 
 // app config//
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/", router);
-
 
 // db connection//
 mongoose.connect(keys.MONGO_URI, { useNewUrlParser: true });
@@ -30,5 +29,16 @@ app.listen(process.env.PORT || 3000, process.env.IP, () => {
   console.log("Server runnning");
 });
 
+/* Routes */
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/status", (req, response) => {
+  response.send({ status: "Running" });
+});
+
+app.use("/user/", userRoutes);
+app.use("/incident/", incidentRoutes);
 
 module.exports = app;
